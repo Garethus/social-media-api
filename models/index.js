d9d8aa36b5d35dd0d2cce7e1134c1c58d580b790
Garethus <reyes.grethelmaec@gmail.com>
2023-06-19 22:03:24 +1000

    updated the models folder

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
-const Student = require('./Student');
-const Course = require('./Course');
+const User = require('./User');
+const Thought = require('./Thought');
+const Response = require('./Response');
 
-module.exports = { Student, Course };
+module.exports = { User, Thought, Response };
