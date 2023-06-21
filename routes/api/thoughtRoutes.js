cd787fc39323ece9e36a396c100e3170bc77c10f
Garethus <reyes.grethelmaec@gmail.com>
2023-06-21 23:05:13 +1000

    corrected the thoughts route

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -14,14 +14,14 @@ router.route('/').get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
 router
-  .route('/thoughts/:thoughtId')
+  .route('/:thoughtId')
   .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
 router
-  .route('/thoughts/:thoughtId/reactions')
+  .route('/:thoughtId/reactions')
   .post(createReaction)
   .delete(deleteReaction);
 
